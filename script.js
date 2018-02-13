6310ccf8d5a2d5e511d06b02583975ea3cb0a4ac
Cassul <aysa.garden@gmail.com>
2018-02-13 14:52:59 +1100

    Commit 3

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var $button = $('.button');
 var $input = $('.color_input');
 var $box = $('.box');
+
+// task 1
 $button.on('click', function (event) {
   event.preventDefault();
   $box.css('background-color', $input.val());
 });
-
+// task 2
 $input.keypress(function (event) {
   var key = event.which;
   if (key === 13) {
   $box.css('background-color', $input.val());
   }
 });
+// task 3
+var i = 0;
+while (i <= 100) {
+  var $div = $('<div></div>');
+  $div = $div.addClass('square');
+  $('body').append($div);
+  i++;
+}
