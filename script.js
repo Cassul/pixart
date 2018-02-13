dd613b3ab8e164deb8083dc0ef3ae270f74ee61c
Cassul <aysa.garden@gmail.com>
2018-02-13 14:40:30 +1100

    Commit 2

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ $button.on('click', function (event) {
   event.preventDefault();
   $box.css('background-color', $input.val());
 });
+
+$input.keypress(function (event) {
+  var key = event.which;
+  if (key === 13) {
+  $box.css('background-color', $input.val());
+  }
+});
