4aec0ed495361a535ca0d078330f08dfd50cb470
Cassul <aysa.garden@gmail.com>
2018-02-13 14:34:27 +1100

    Commit 1

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var $button = $('.button');
 var $input = $('.color_input');
 var $box = $('.box');
 $button.on('click', function (event) {
-  console.log('hello');
+  event.preventDefault();
   $box.css('background-color', $input.val());
 });
